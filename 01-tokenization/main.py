5c392ec138fefe36665a5e0e5b53abb04f2075b7
aleem-dev <aleem_shaikh786@yahoo.com>
2025-06-20 19:37:52 -0700

    token encoding and decoding

--- 01-tokenization/main.py
+++ 01-tokenization/main.py
@@ -3,6 +3,8 @@ import tiktoken
 enc = tiktoken.encoding_for_model("gpt-4o") #getting the encoder gpt-4o
 
 text = "Hello, I am Aleem Shaikh"
-tokens = enc.encode(text)
-
+tokens = enc.encode(text)   #create tokens
 print("Tokens:", tokens)
+
+decoded = enc.decode(tokens)    #decoded the tokens
+print("Decoded:", decoded)
