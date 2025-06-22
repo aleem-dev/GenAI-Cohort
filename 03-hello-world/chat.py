88b14932bdfc1e11f24b29cfbf1c3f4d3d013cbe
aleem-dev <aleem_shaikh786@yahoo.com>
2025-06-22 13:04:07 -0700

    set up chat bot, and env variables

--- 03-hello-world/chat.py
+++ 03-hello-world/chat.py
@@ -1,3 +1,19 @@
+from dotenv import load_dotenv
 from openai import OpenAI
 
+load_dotenv()
+
 client = OpenAI()
+
+response = client.chat.completions.create(
+    model="gpt-4.1-mini",
+    messages=[
+        {"role":"user","content":"My name is aleem"},
+        {"role":"assistant","content":"Hi Aleem"},
+        {"role":"user","content":"What is my name?"},
+        {"role":"assistant","content":"Your name is Aleem."},
+        {"role":"user","content":"How are you?"},
+    ]
+)
+
+print(response.choices[0].message.content) 
