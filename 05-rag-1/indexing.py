5adc4be7bf204412adeed58f916a67833eda05dc
aleem-dev <aleem_shaikh786@yahoo.com>
2025-11-14 11:21:52 -0800

    Service to parse large pdf is ready for deployment

---
+++ 05-rag-1/indexing.py
@@ -0,0 +1,37 @@
+from langchain_community.document_loaders import PyPDFLoader #loading
+from pathlib import Path  
+from langchain_text_splitters import RecursiveCharacterTextSplitter #Chunking
+from langchain_openai import OpenAIEmbeddings   #Vector Embedding
+from dotenv import load_dotenv
+from langchain_qdrant import QdrantVectorStore
+
+load_dotenv()
+
+#Loading
+pdf_path = Path(__file__).parent / "nodejs.pdf"
+loader = PyPDFLoader(file_path=pdf_path)
+docs = loader.load() # Read pdf file
+
+# print(docs[124])
+
+#Chunking
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=200, add_start_index=True
+)
+all_splits = text_splitter.split_documents(docs)
+
+# print(len(all_splits))
+# print(all_splits[5].page_content)
+
+#Vector embedings
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+
+# Using [embedding_model] create embeddings of [split_docs] and store in DB
+vector_store = QdrantVectorStore.from_documents(
+    documents=all_splits,
+    url="http://localhost:6333/", #database url
+    collection_name = "learning_vector", #database collection name
+    embedding=embeddings
+)
+
+print("indexing of documents is done")
